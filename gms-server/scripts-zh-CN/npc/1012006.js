909d8d1663c67cf0ea7fefee3cbca18f7d5b190b
Xiaojie Zhou <hiugiak@outlook.com>
2024-10-08 01:42:21 +0800

    feat: 宠物公园学习群宠技能

--- gms-server/scripts-zh-CN/npc/1012006.js
+++ gms-server/scripts-zh-CN/npc/1012006.js
@@ -1,8 +1,8 @@
 /*
-	This file is part of the OdinMS Maple Story Server
+    This file is part of the OdinMS Maple Story Server
     Copyright (C) 2008 Patrick Huy <patrick.huy@frz.cc>
-		       Matthias Butz <matze@odinms.de>
-		       Jan Christian Meyer <vimes@odinms.de>
+               Matthias Butz <matze@odinms.de>
+               Jan Christian Meyer <vimes@odinms.de>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
@@ -20,14 +20,14 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 /* Author: Xterminator, Moogra
-	NPC Name: 		Trainer Bartos
-	Map(s): 		Victoria Road : Pet-Walking Road (100000202)
-	Description: 		Pet Trainer
+    NPC Name: 		Trainer Bartos
+    Map(s): 		Victoria Road : Pet-Walking Road (100000202)
+    Description: 		Pet Trainer
 */
 var status = 0;
 
 function start() {
-    cm.sendSimple("你有什么事情找我吗？\r\n#L0##b请告诉我关于这个地方的情况。#l\r\n#L1#我是因为仙子玛尔的话才来这里的…#k#l");
+    cm.sendSimple("你有什么事情找我吗？\r\n#L0##b请告诉我关于这个地方的情况。#l\r\n#L1#我是因为仙子玛尔的话才来这里的…#k#l\r\n#L2##b学习群宠技能#k#l");
 }
 
 function action(mode, type, selection) {
@@ -39,16 +39,26 @@ function action(mode, type, selection) {
     } else {
         status++;
         if (status == 1) {
-            if (selection == 0) {
-                if (cm.haveItem(4031035)) {
-                    cm.sendNext("拿到那封信，和你的宠物一起跳过障碍物，把那封信带给我的兄弟弗罗德训练师。把信交给他，你的宠物会有好事发生。");
+            switch (selection) {
+                case 0:
+                    if (cm.haveItem(4031035)) {
+                        cm.sendNext("拿到那封信，和你的宠物一起跳过障碍物，把那封信带给我的兄弟弗罗德训练师。把信交给他，你的宠物会有好事发生。");
+                        cm.dispose();
+                    } else {
+                        cm.sendYesNo("这是你可以带宠物散步的路。你可以和它一起四处走动，或者训练它通过这里的障碍。如果你和宠物还不够亲近，可能会出现问题，它就不会像你想的那样听从你的命令。那么，你觉得呢？想要训练你的宠物吗？");
+                    }
+                    break;
+                case 1:
+                    cm.sendOk("嘿，你确定你见过#b仙灵玛尔#k吗？如果你以前从未见过她，不要对我撒谎，因为很明显。那甚至不是一个好谎言！");
                     cm.dispose();
-                } else {
-                    cm.sendYesNo("这是你可以带宠物散步的路。你可以和它一起四处走动，或者训练它通过这里的障碍。如果你和宠物还不够亲近，可能会出现问题，它就不会像你想的那样听从你的命令。那么，你觉得呢？想要训练你的宠物吗？");
-                }
-            } else {
-                cm.sendOk("嘿，你确定你见过#b仙灵玛尔#k吗？如果你以前从未见过她，不要对我撒谎，因为很明显。那甚至不是一个好谎言！");
-                cm.dispose();
+                    break;
+                case 2:
+                    cm.teachSkill(8, 1, 1, -1);   //polearm mastery
+                    cm.dispose();
+                    break;
+                default:
+                    cm.dispose();
+                    break;
             }
         } else if (status == 2) {
             cm.gainItem(4031035, 1);
